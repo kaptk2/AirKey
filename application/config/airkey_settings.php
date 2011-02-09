9ce2b0a16384989a838b5e3e958c2facc1be6cb9
Andrew Niemantsverdriet <andrewNiemants@gmail.com>
2011-02-09 14:34:12 -0700

    Rebased AirKey on CodeIgniter 2.0

--- application/config/airkey_settings.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-  $config['networkName'] = "My Network";
-  $config['networkPassword'] = "1234";
-?>
--- application/config/autoload.php
+++ application/config/autoload.php
@@ -18,9 +18,9 @@
 |
 | These are the things you can load automatically:
 |
-| 1. Libraries
-| 2. Helper files
-| 3. Plugins
+| 1. Packages
+| 2. Libraries
+| 3. Helper files
 | 4. Custom config files
 | 5. Language files
 | 6. Models
@@ -29,17 +29,30 @@
 
 /*
 | -------------------------------------------------------------------
+|  Auto-load Packges
+| -------------------------------------------------------------------
+| Prototype:
+|
+|  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
+|
+*/
+
+$autoload['packages'] = array(APPPATH.'third_party');
+
+
+/*
+| -------------------------------------------------------------------
 |  Auto-load Libraries
 | -------------------------------------------------------------------
 | These are the classes located in the system/libraries folder
-| or in your system/application/libraries folder.
+| or in your application/libraries folder.
 |
 | Prototype:
 |
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array();
 
 
 /*
@@ -51,19 +64,7 @@ $autoload['libraries'] = array('database');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
-
-
-/*
-| -------------------------------------------------------------------
-|  Auto-load Plugins
-| -------------------------------------------------------------------
-| Prototype:
-|
-|	$autoload['plugin'] = array('captcha', 'js_calendar');
-*/
-
-$autoload['plugin'] = array();
+$autoload['helper'] = array();
 
 
 /*
@@ -79,7 +80,7 @@ $autoload['plugin'] = array();
 |
 */
 
-$autoload['config'] = array('airkey_settings');
+$autoload['config'] = array();
 
 
 /*
@@ -111,6 +112,5 @@ $autoload['language'] = array();
 $autoload['model'] = array();
 
 
-
 /* End of file autoload.php */
-/* Location: ./system/application/config/autoload.php */
+/* Location: ./application/config/autoload.php */

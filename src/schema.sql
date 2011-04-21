9cf7604dbca9d32c94c6c74be390878584e48413
Andrew Niemantsverdriet <andrewNiemants@gmail.com>
2011-04-21 14:44:29 -0600

    Added new table to record the packages

--- src/schema.sql
+++ src/schema.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS administrator;
 DROP TABLE IF EXISTS module_files;
 DROP TABLE IF EXISTS module_commands;
+DROP TABLE IF EXISTS module_packages;
 DROP TABLE IF EXISTS modules;
 DROP TABLE IF EXISTS ap;
 
@@ -32,14 +33,12 @@ CREATE TABLE associates (
 	FOREIGN KEY (group_name) REFERENCES groups(group_name) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-
 CREATE TABLE administrator (
 	user_name VARCHAR(255) NOT NULL PRIMARY KEY,
 	password VARCHAR(255) NOT NULL,
 	real_name VARCHAR(255),
 	description TEXT
 ) ENGINE=InnoDB;
-
 
 CREATE TABLE heartbeat (
 	mac CHAR(12) NOT NULL PRIMARY KEY,
@@ -55,15 +54,23 @@ CREATE TABLE modules (
 ) ENGINE=InnoDB;
 
 CREATE TABLE module_files (
-	module_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	module_name VARCHAR(255) NOT NULL,
 	remote_file VARCHAR(255) NOT NULL,
 	local_file VARCHAR(255) NOT NULL,
 	FOREIGN KEY (module_name) REFERENCES modules(module_name) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 CREATE TABLE module_commands (
-	module_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	module_name VARCHAR(255) NOT NULL,
 	command VARCHAR(255),
+	FOREIGN KEY (module_name) REFERENCES modules(module_name) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE module_packages (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	module_name VARCHAR(255) NOT NULL,
 	package_name VARCHAR(255),
 	FOREIGN KEY (module_name) REFERENCES modules(module_name) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;

d6ade569fb206a6a7188b5d6f2bb2a9a4994df59
Andrew Niemantsverdriet <andrewniemants@gmail.com>
2011-09-13 09:59:57 -0600

    Small Clean Up

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -3,23 +3,24 @@
 	{
 		function index()
 		{
+			$this->load->model('heartbeat_model');
+			$this->load->model('group_model');
+			$this->load->model('ap_model');
+
 			if ($_POST) //make sure that data has been posted
 			{
 				$search_term = $this->input->post('search_term');
 				$search_term = $this->security->xss_clean($search_term);
 				// Search APs
-				$this->load->model('ap_model');
 				$data['ap_results'] = $this->ap_model->apSearch($search_term);
 				// Search Groups
-				$this->load->model('group_model');
 				$data['group_results'] = $this->group_model->groupSearch($search_term);
 
 				// Load View to show search results
 				$menu['page_name'] = ""; //pass empty page name
 				$menu['total_AP'] = $this->ap_model->activeAPCount();
 				$menu['pending'] = $this->ap_model->pendingCommand();
-				$menu['network_status'] = "A OK"; //TODO
-
+				$menu['network_status'] = $this->heartbeat_model->countTroubleAP();
 				// Build Search Page
 				$this->load->view('header_view');
 				$this->load->view('menu_view', $menu);
